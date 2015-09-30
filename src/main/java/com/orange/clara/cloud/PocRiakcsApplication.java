b869b023db392f14d3eb80d3363b2ea0c04ef213
Arthur Halet <arthur.halet@orange.com>
2015-09-30 16:17:19 +0200

    first commit for poc-riakcs

---
+++ src/main/java/com/orange/clara/cloud/PocRiakcsApplication.java
@@ -0,0 +1,12 @@
+package com.orange.clara.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocRiakcsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PocRiakcsApplication.class, args);
+    }
+}
---
+++ src/main/java/com/orange/clara/cloud/controller/PocRiakcsController.java
@@ -0,0 +1,79 @@
+package com.orange.clara.cloud.controller;
+
+import org.jclouds.blobstore.BlobStore;
+import org.jclouds.blobstore.BlobStoreContext;
+import org.jclouds.blobstore.domain.Blob;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * Copyright (C) 2015 Orange
+ * <p/>
+ * This software is distributed under the terms and conditions of the 'MIT'
+ * license which can be found in the file 'LICENSE' in this package distribution
+ * or at 'http://opensource.org/licenses/MIT'.
+ * <p/>
+ * Author: Arthur Halet
+ * Date: 30/09/2015
+ */
+@RestController
+public class PocRiakcsController {
+
+    @Autowired
+    private ResourceLoader resourceLoader;
+
+    @Autowired
+    @Qualifier(value = "blobStoreContext")
+    private BlobStoreContext blobStoreContext;
+
+    @Autowired
+    @Qualifier(value = "bucketName")
+    private String bucketName;
+
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    public
+    @ResponseBody
+    String upload(@RequestParam("name") String name,
+                  @RequestParam("file") MultipartFile multipartFile) throws IOException {
+        if (multipartFile.isEmpty()) {
+            return "You failed to upload " + name + " because the file was empty.";
+        }
+
+        byte[] bytes = multipartFile.getBytes();
+
+        BlobStore blobStore = blobStoreContext.getBlobStore();
+        Blob blob = blobStore.blobBuilder(name)
+                .payload(bytes)
+                .build();
+
+        blobStore.putBlob(this.bucketName, blob);
+
+        return "We uploaded the file '" + name + "' to a riakcs!";
+    }
+
+    @RequestMapping("/show/{fileName:.*}")
+    public String show(@PathVariable String fileName) throws IOException {
+        BlobStore blobStore = blobStoreContext.getBlobStore();
+        Blob blob = blobStore.getBlob(this.bucketName, fileName);
+        System.out.println(fileName);
+        InputStream inputStream = blob.getPayload().openStream();
+
+        String content = "";
+        BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+        for (String line = br.readLine(); line != null; line = br.readLine()) {
+            content += line;
+            content += "<br/>";
+        }
+        br.close();
+
+        return content;
+    }
+}
